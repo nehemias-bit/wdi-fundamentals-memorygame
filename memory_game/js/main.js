db38fac9a5c954b9bc4bf079abd516d33583ec73
Nehemias Cruz <nehemiascruz@Nehemiass-MacBook-Pro.local>
2019-08-27 11:28:44 -0400

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+let cardOne = "Queen";
+let cardTwo = "Queen";
+let cardThree = "King";
+let cardFour = "King";
+
+console.log("User flipped" + " " + cardTwo);
+console.log("User flipped" + " " + cardFour);
