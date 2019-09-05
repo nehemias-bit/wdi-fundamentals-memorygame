95a65e190911139d3f5b49f9f0098e6da467a96b
Nehemias Cruz <nehemiascruz@Nehemiass-MacBook-Pro.local>
2019-09-05 13:16:15 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -15,9 +15,19 @@ rank: "king",
 suit: "diamonds",
 cardImage: "images/king-of-diamonds.png"
 }];
+
 let cardsInPlay = [];
 
-function checkForMatch() {
+// function resetGame() { 
+//     document.getElementById('game-board').reset();
+// }
+
+function flipCard() {
+    let cardId = this.getAttribute('data-id');
+    console.log("User flipped " + cards[cardId].rank);
+    cardsInPlay.push(cards[cardId].rank);
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
     if(cardsInPlay.length === 2){
         if(cardsInPlay[0] === cardsInPlay[1]){
             alert("You found a match!");
@@ -26,15 +36,32 @@ function checkForMatch() {
             alert("Sorry, try again.");
         }
     }
-}
+    this.setAttribute('src', cards[cardId].cardImage);
+};
 
-function flipCard(cardId) {
-    console.log("User flipped " + cards[cardId].rank);
-    cardsInPlay.push(cards[cardId].rank);
-    console.log(cards[cardId].cardImage);
-    console.log(cards[cardId].suit);   
-    checkForMatch();
-}
+function createBoard() {
+    for(let i = 0; i < cards.length; i++){
+        let cardElement = document.createElement('img');
+        cardElement.setAttribute('src', 'images/back.png');
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipCard);
+        document.getElementById('game-board').appendChild(cardElement);
+    }
+    // let game = document.getElementById('game-board');
+    // game.querySelector('.resetButton').addEventListener('click', resetGame);
+};
 
-flipCard(0);
-flipCard(2);
+// document.getElementsByClassName('resetButton').onclick = function (){
+//     document.getElementById('game-board').reset();
+// }
+
+createBoard();
+// resetGame();
+
+
+
+//The way I implemented the reset button is not the way you wanted me to do it 
+//because I didn't use javascript. I really just couldn't figure it out. I also didn't want 
+//to hand in the project without a working reset button so I went ahead and just made it
+//work the cheap way using HTML. I left my attempts at making the reset button with javascript 
+// commented out in this js file so that we can maybe discuss it in the future.
